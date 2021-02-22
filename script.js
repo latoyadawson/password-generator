d6bb1491e11375a6562008b1a111f5ccc6db2567
Latoya Dawson <lmdw13@yahoo.com>
2021-02-22 14:29:38 -0800

    correcting functionality

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 
+
 //function for random elements
 const randomFunc = {
 	lower: getRandomLower,
@@ -15,7 +16,7 @@ const randomFunc = {
 	symbol: getRandomSymbol
 }
 
-//generate password function 
+//generate password function  
 function generatePassword(lower, upper, number, symbol, length) {
 	let generatedPassword = '';
 	const typesCount = lower + upper + number + symbol;
@@ -26,7 +27,7 @@ function generatePassword(lower, upper, number, symbol, length) {
 		return '';
 	}
 	
-	// create a loop
+	// create a loop to go through different  passowrd requirements
 	for(let i=0; i<length; i+=typesCount) {
 		typesArr.forEach(type => {
 			const funcName = Object.keys(type)[0];
@@ -39,6 +40,7 @@ function generatePassword(lower, upper, number, symbol, length) {
 	return finalPassword;
 }
 
+//generator random requirements - http:www.net-comber.com/charset.html
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -55,16 +57,26 @@ function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.'
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
-// check to see if check box is clicked. '
-function validate() {
-  if (uppercase.checked == false || lowercase.checked == false || numbers.checked == false || symbols.checked == false ) {
-    alert( "You need to check at least 1 field to generate a password" );
-    return false;
-  }
+
+  
+//function to present passowrd criteria on click 
+function passwordCriteria () {
+	// generate the div with class of requirements
+	document.getElementById('criteria').style.visibility ="visible";
+	document.getElementById('criteria').style.display ="block";
 }
 
-//generate event
-generate.addEventListener('click', () => {
+
+// check to see if check box is clicked. '
+function validate() {
+	if (!uppercase.checked && !lowercase.checked && !numbers.checked && !symbols.checked) {
+	  alert( "You need to check at least 1 field to generate a password" );
+	  return false;
+	}
+}
+
+//function for generating password 
+function passwordGenerator () {
 	const length = +lengthEl.value;
 	const hasLower = lowercaseEl.checked;
 	const hasUpper = uppercaseEl.checked;
@@ -72,9 +84,25 @@ generate.addEventListener('click', () => {
 	const hasSymbol = symbolsEl.checked;
 	
 
-  resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+  	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 
-  validate();
+  
+}
+//generate event
+generate.addEventListener('click', () => {
+	//1st click call passwordCriterai fucntion to appear 
+	if (document.getElementById('criteria').style.visibility != "visible") {
+		passwordCriteria();
+	} else {
+		//2nd click validate that one of the minimum of 1 criteria has been checked / If it has generate password /else generate alert prompt
+		validate();
+		passwordGenerator();
+	}
+	
+
+
+	
+	
 });
 
 
